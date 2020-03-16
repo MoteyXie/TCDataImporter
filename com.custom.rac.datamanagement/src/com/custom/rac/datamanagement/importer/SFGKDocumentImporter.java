5409f32bde9f621fc9da4437d3e5287851ee5ffe
DeveLuo <654333440@qq.com>
2020-03-16 13:39:08 +0800

    修改读取excel值错位的BUG

--- com.custom.rac.datamanagement/src/com/custom/rac/datamanagement/importer/SFGKDocumentImporter.java
+++ com.custom.rac.datamanagement/src/com/custom/rac/datamanagement/importer/SFGKDocumentImporter.java
@@ -1,6 +1,8 @@
 package com.custom.rac.datamanagement.importer;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.custom.rac.datamanagement.util.AbstractImporter;
 import com.custom.rac.datamanagement.util.MyClassifyManager;
@@ -9,17 +11,21 @@ import com.custom.rac.datamanagement.util.MyStatusUtil;
 import com.custom.rac.datamanagement.util.PropertyContainer;
 import com.teamcenter.rac.aifrcp.AIFUtility;
 import com.teamcenter.rac.kernel.TCComponent;
+import com.teamcenter.rac.kernel.TCComponentFolder;
+import com.teamcenter.rac.kernel.TCComponentFolderType;
 import com.teamcenter.rac.kernel.TCComponentItemType;
 import com.teamcenter.rac.kernel.TCComponentUser;
 import com.teamcenter.rac.kernel.TCComponentUserType;
+import com.teamcenter.rac.kernel.TCException;
 import com.teamcenter.rac.kernel.TCSession;
+import com.teamcenter.rac.util.MessageBox;
 
 public class SFGKDocumentImporter extends AbstractImporter {
 
 	String shared_directory_path = "\\\\192.168.25.11";
 	TCSession session = (TCSession) AIFUtility.getDefaultSession();
 	MyClassifyManager cls_manger = new MyClassifyManager(session);
-	
+	TCComponentFolder folder = null;
 	
 	@Override
 	public String getName() {
@@ -45,31 +51,35 @@ public class SFGKDocumentImporter extends AbstractImporter {
 
 	@Override
 	public void onSingleFinish(int index, TCComponent tcc) throws Exception {
-		
+		if (folder != null) {
+			folder.add("contents", tcc);
+		}
 	}	
 
 	@Override
 	public void onSingleError(int index, Exception e) {
-		e.printStackTrace();
-		
+		System.out.println("第" +index+ "行异常：" + e.getMessage());
 	}
 
 	@Override
-	public void onStart() {
-		
+	public void onStart() throws TCException {
+		TCComponentFolderType folderType = (TCComponentFolderType) session.getTypeComponent("Folder");
+		Date date = new Date();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		String name = "图纸历史数据导入" + format.format(date);
+		folder = folderType.create(name, "", "Folder");
+		session.getUser().getHomeFolder().add("contents", folder);
 	}
 
 	@Override
 	public void onFinish() {
-		System.out.println("导入完成！");
+		MessageBox.post("导入完成","提示", MessageBox.INFORMATION);
 	}
 
 	@Override
 	public boolean ignoreProperty(int index, String propertyDisplayName) {
 		if (propertyDisplayName.equals("文档编号") || propertyDisplayName.equals("版本") 
-			||propertyDisplayName.equals("文档名称") || propertyDisplayName.equals("文档分类ID")
-			|| propertyDisplayName.equals("电子档存放地址") || propertyDisplayName.equals("所有者")
-			|| propertyDisplayName.equals("文档状态")){
+			||propertyDisplayName.equals("文档名称")){
 			return true;
 		}
 		return false;
@@ -110,21 +120,18 @@ public class SFGKDocumentImporter extends AbstractImporter {
 	}
 		
 	@Override
-	public void onPropertyRealNameNotFound(int index, String propertyName) {
-		
-		
+	public void onPropertyRealNameNotFound(int index, String propertyName) {		
+		System.out.println("第：" + index + "行的（" + propertyName + "）属性不存在！");
 	}
 
 	@Override
 	public void onSetPropertyFinish(int index, String propertyDisplayName) {
-		System.out.println("第" + index + "行");
 		
 	}
 
 	@Override
 	public void onSetPropertyError(int index, String propertyDisplayName, Exception e) {
-		e.printStackTrace();
-		
+		System.out.println("第" +index+ "行异常：" + e.getMessage());
 	}
 
 	@Override
--- com.custom.rac.datamanagement/src/com/custom/rac/datamanagement/test/Test.java
+++ com.custom.rac.datamanagement/src/com/custom/rac/datamanagement/test/Test.java
@@ -1,5 +1,6 @@
 package com.custom.rac.datamanagement.test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
@@ -13,9 +14,13 @@ public class Test {
 	public static void main(String[] args) {
 		
 //		tableViewPart.loadExcel(file);
-		ExcelEventParser eep = new ExcelEventParser("D:\\01_我的工作\\小熊电器\\01工作区\\04系统设计\\历史数据导入（测试数据）\\【小熊电器 PLM】电子件物料历史数据整理模板v1.1&nbsp; 数据20180829.xlsx");
-		MyTable myTable = eep.parse();
-		
-		System.out.println(myTable);
+//		ExcelEventParser eep = new ExcelEventParser("D:\\01_我的工作\\小熊电器\\01工作区\\04系统设计\\历史数据导入（测试数据）\\【小熊电器 PLM】电子件物料历史数据整理模板v1.1&nbsp; 数据20180829.xlsx");
+//		MyTable myTable = eep.parse();
+//		
+//		System.out.println(myTable);
+		List<String> list = new ArrayList<>(6);
+		list.add(0, "yyy");
+		list.add(1, "yyy");
+		list.add(3, "yyy");
 	}
 }
