eecc90dcd0ecb3fd6f6ff5c78a0ae15002ea5268
Motey <Motey@foxmail.com>
2020-03-17 17:17:20 +0800

    新增导入结果在界面显示功能， 新增保存数据和导出表格功能

---
+++ com.custom.rac.datamanagement/src/com/custom/rac/datamanagement/action/ExportTableAction.java
@@ -0,0 +1,26 @@
+package com.custom.rac.datamanagement.action;
+
+import org.eclipse.swt.widgets.Widget;
+
+import com.custom.rac.datamanagement.util.AbstractTableAction;
+import com.custom.rac.datamanagement.views.ExcelTableViewPart;
+
+public class ExportTableAction extends AbstractTableAction {
+
+	public ExportTableAction(ExcelTableViewPart tableViewPart) {
+		super(tableViewPart);
+	}
+
+	@Override
+	public void run(Widget widget) throws Exception {
+
+		//先判断程序是否在进行中
+		boolean isExecuting = tableViewPart.isExecuting();
+		if(isExecuting) {
+			throw new Exception("程序执行中，无法导出数据！");
+		}
+		
+		String lastSelectedFilePath = OpenFileAction.lastSelectFile;
+	}
+
+}
