576edb5d410140c2099cf87263f1a67a579ff75d
Erkin-github <2507451951@qq.com>
2020-04-03 16:29:39 +0800

    封装类

--- com.custom.rac.datamanagement/src/com/custom/rac/datamanagement/action/SaveResultAction.java
+++ com.custom.rac.datamanagement/src/com/custom/rac/datamanagement/action/SaveResultAction.java
@@ -1,22 +1,9 @@
 package com.custom.rac.datamanagement.action;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFCellStyle;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Widget;
 
-import com.custom.rac.datamanagement.swtxls.MySheet;
-import com.custom.rac.datamanagement.swtxls.SWTSheet;
 import com.custom.rac.datamanagement.util.AbstractTableAction;
+import com.custom.rac.datamanagement.util.WriteDataToExcel;
 import com.custom.rac.datamanagement.views.ExcelTableViewPart;
 import com.teamcenter.rac.util.MessageBox;
 
@@ -28,8 +15,6 @@ public class SaveResultAction extends AbstractTableAction {
 
 	@Override
 	public void run(Widget widget) throws Exception {
-		InputStream input = null;
-		OutputStream out = null;
 		// 先判断程序是否在进行中
 		tableViewPart.setExecuting(false);
 		boolean isExecuting = tableViewPart.isExecuting();
@@ -38,53 +23,8 @@ public class SaveResultAction extends AbstractTableAction {
 		}
 		// 文件打开路径
 		String lastSelectedFilePath = OpenFileAction.lastSelectFile;
-
-		SWTSheet swtSheet = tableViewPart.getSWTWorkbook().getSelectedSheet();
-		Table table = swtSheet.getTable();// 获取表格对象
-		int tcol = table.getColumnCount();// 获取表格列数
-		int trow = table.getItemCount();// 获取表格行数
-
-		MySheet mySheet = swtSheet.getSheet();
-		String name = mySheet.name;
-		int scol = mySheet.getColumnNum();
-		try {
-			input = new FileInputStream(lastSelectedFilePath);
-			XSSFWorkbook wb = new XSSFWorkbook(input);
-			XSSFSheet sheet = wb.getSheet(name);
-			XSSFRow row = sheet.getRow(0);
-			XSSFCell cell = row.createCell(scol);
-			XSSFCellStyle cellStyle = wb.createCellStyle();
-			cellStyle.setBorderBottom(XSSFCellStyle.BORDER_THIN); // 下边框
-			cellStyle.setBorderLeft(XSSFCellStyle.BORDER_THIN);// 左边框
-			cellStyle.setBorderTop(XSSFCellStyle.BORDER_THIN);// 上边框
-			cellStyle.setBorderRight(XSSFCellStyle.BORDER_THIN);// 右边框
-			cellStyle.setWrapText(true);
-			sheet.setColumnWidth(scol, 256 * 50 + 184);// 设置列宽
-			cell.setCellStyle(cellStyle);
-			for (int i = 1; i < trow; i++) {
-				for (int j = 2; j < tcol; j++) {
-					TableItem tableItem = table.getItem(i);// 获取第i行数据
-					String value = tableItem.getText(j);// 获取第j列数据
-					row = sheet.getRow(i);
-					cell = row.createCell(j - 2);
-					cell.setCellValue(value);
-					cell.setCellStyle(cellStyle);
-				}
-			}
-			out = new FileOutputStream(lastSelectedFilePath);
-			wb.write(out);
-			MessageBox.post("保存成功，保存路径：" + lastSelectedFilePath, "提示", MessageBox.INFORMATION);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (input != null) {
-				input.close();
-			}
-			if (out != null) {
-				out.close();
-			}
-		}
+		WriteDataToExcel.WriteData(tableViewPart, lastSelectedFilePath, lastSelectedFilePath);
+		MessageBox.post("保存成功，保存路径：" + lastSelectedFilePath, "提示", MessageBox.INFORMATION);
 
 	}
 
---
+++ com.custom.rac.datamanagement/src/com/custom/rac/datamanagement/util/WriteDataToExcel.java
@@ -0,0 +1,65 @@
+package com.custom.rac.datamanagement.util;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+
+import com.custom.rac.datamanagement.swtxls.MySheet;
+import com.custom.rac.datamanagement.swtxls.SWTSheet;
+import com.custom.rac.datamanagement.views.ExcelTableViewPart;
+
+public class WriteDataToExcel {
+	public static void WriteData(ExcelTableViewPart tableViewPart, String lastSelectedFilePath, String filePath)
+			throws Exception {
+		InputStream input = null;
+		OutputStream out = null;
+		SWTSheet swtSheet = tableViewPart.getSWTWorkbook().getSelectedSheet();
+		Table table = swtSheet.getTable();// 获取表格对象
+		int tcol = table.getColumnCount();// 获取表格列数
+		int trow = table.getItemCount();// 获取表格行数
+
+		MySheet mySheet = swtSheet.getSheet();
+		String name = mySheet.name;
+		int scol = mySheet.getColumnNum();
+		input = new FileInputStream(lastSelectedFilePath);
+		XSSFWorkbook wb = new XSSFWorkbook(input);
+		XSSFSheet sheet = wb.getSheet(name);
+		XSSFRow row = sheet.getRow(0);
+		XSSFCell cell = row.createCell(scol);
+		XSSFCellStyle cellStyle = wb.createCellStyle();
+		cellStyle.setBorderBottom(XSSFCellStyle.BORDER_THIN); // 下边框
+		cellStyle.setBorderLeft(XSSFCellStyle.BORDER_THIN);// 左边框
+		cellStyle.setBorderTop(XSSFCellStyle.BORDER_THIN);// 上边框
+		cellStyle.setBorderRight(XSSFCellStyle.BORDER_THIN);// 右边框
+		cellStyle.setWrapText(true);
+		sheet.setColumnWidth(scol, 256 * 50 + 184);// 设置列宽
+		cell.setCellStyle(cellStyle);
+		for (int i = 1; i < trow; i++) {
+			for (int j = 2; j < tcol; j++) {
+				TableItem tableItem = table.getItem(i);// 获取第i行数据
+				String value = tableItem.getText(j);// 获取第j列数据
+				row = sheet.getRow(i);
+				cell = row.createCell(j - 2);
+				cell.setCellValue(value);
+				cell.setCellStyle(cellStyle);
+			}
+		}
+		out = new FileOutputStream(filePath);
+		wb.write(out);
+		if (input != null) {
+			input.close();
+		}
+		if (out != null) {
+			out.close();
+		}
+	}
+}
